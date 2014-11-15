75bdbe3fbcc2f85bc16b00ec84d8782f77b5037d
jarjun <arjun.jain@duke.edu>
2014-11-15 16:05:26 -0500

    initial push

---
+++ app/views.py
@@ -0,0 +1,53 @@
+from flask import render_template
+from app import app
+import requests
+from twilio.rest import TwilioRestClient
+'''
+@app.route('/')
+def default():
+	return render_template("rootpage.html")
+
+@app.route('/<name>')
+def index(name):
+	p = {"limit": 50}
+	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
+	r = r.json()
+	channellist = []
+	for key in r["follows"]:
+		streamname = key["channel"]["name"]
+		stream = requests.get("https://api.twitch.tv/kraken/streams/" + streamname)
+		stream = stream.json()
+		if stream["stream"] != None:
+			channellist.append((stream["stream"]["viewers"], streamname))
+
+	channellist = sorted(channellist)
+	fin = channellist[-1][1]
+	return render_template("stream.html", top = fin)
+
+@app.route('/<name>/<pos>')
+def indexpos(name,pos):
+	p = {"limit": 50}
+	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
+	r = r.json()
+	channellist = []
+	for key in r["follows"]:
+		streamname = key["channel"]["name"]
+		stream = requests.get("https://api.twitch.tv/kraken/streams/" + streamname)
+		stream = stream.json()
+		if stream["stream"] != None:
+			channellist.append((stream["stream"]["viewers"], streamname))
+
+	channellist = sorted(channellist)
+	fin = channellist[-int(pos)][1]
+	return render_template("stream.html", top = fin)
+
+'''
+@app.route('/', methods=['GET', 'POST'])
+def default():
+	account_sid = "ACed174aa4db08574d608df749cd16e3fd"
+	auth_token  = "d96a5e6b2722cac3116e0298c965efd0"
+	client = TwilioRestClient(account_sid, auth_token)
+	messages = client.messages.list()
+	return render_template("rootpage.html", check = messages[0].body)
+
+
