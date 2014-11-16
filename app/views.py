cd1d901f2cbc8e10595bc7f978411b20c938f630
jarjun <arjun.jain@duke.edu>
2014-11-16 06:43:23 -0500

    test ip

--- app/views.py
+++ app/views.py
@@ -52,7 +52,7 @@ def submitted():
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-	ip = request.environ['REMOTE_ADDR']
+	ip = request.headers.get('X-Real-IP')
 	return ip
 	form = RequestForm()
 	if form.validate_on_submit():
