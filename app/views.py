8fa41613dc40a44c98ff54fa0bf867c6114f911d
jarjun <arjun.jain@duke.edu>
2014-11-15 19:28:13 -0500

    form sends to number hopefully

--- app/views.py
+++ app/views.py
@@ -4,51 +4,13 @@ from .forms import RequestForm
 import requests
 from twilio.rest import TwilioRestClient
 import twilio.twiml
-'''
-@app.route('/')
-def default():
-	return render_template("rootpage.html")
 
-@app.route('/<name>')
-def index(name):
-	p = {"limit": 50}
-	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
-	r = r.json()
-	channellist = []
-	for key in r["follows"]:
-		streamname = key["channel"]["name"]
-		stream = requests.get("https://api.twitch.tv/kraken/streams/" + streamname)
-		stream = stream.json()
-		if stream["stream"] != None:
-			channellist.append((stream["stream"]["viewers"], streamname))
+account_sid = "ACed174aa4db08574d608df749cd16e3fd"
+auth_token  = "d96a5e6b2722cac3116e0298c965efd0"
+client = TwilioRestClient(account_sid, auth_token)
 
-	channellist = sorted(channellist)
-	fin = channellist[-1][1]
-	return render_template("stream.html", top = fin)
-
-@app.route('/<name>/<pos>')
-def indexpos(name,pos):
-	p = {"limit": 50}
-	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
-	r = r.json()
-	channellist = []
-	for key in r["follows"]:
-		streamname = key["channel"]["name"]
-		stream = requests.get("https://api.twitch.tv/kraken/streams/" + streamname)
-		stream = stream.json()
-		if stream["stream"] != None:
-			channellist.append((stream["stream"]["viewers"], streamname))
-
-	channellist = sorted(channellist)
-	fin = channellist[-int(pos)][1]
-	return render_template("stream.html", top = fin)
-
-'''
 @app.route('/', methods=['GET', 'POST'])
 def default():
-	account_sid = "ACed174aa4db08574d608df749cd16e3fd"
-	auth_token  = "d96a5e6b2722cac3116e0298c965efd0"
-	client = TwilioRestClient(account_sid, auth_token)
 	toGet = request.values.get('Body',None)
 	resp = twilio.twiml.Response()
 	message = client.messages.create(to="+17572823575", from_ = "+12039874014",body=toGet)
@@ -60,9 +22,9 @@ def form():
 	form = RequestForm()
 	if form.validate_on_submit():
 		#flash(form.address.data)
-
-		#SEND MESSAGE HERE
-
+		resp = twilio.twiml.Response()
+		message = client.messages.create(to="+17572823575", from_ = "+12039874014",body=form.address.data)
+		#resp.message(message)
 	return render_template('request.html',
 							title= 'Request',
 							form= form)
