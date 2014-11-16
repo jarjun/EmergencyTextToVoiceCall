057f1b2cc04ae8b2876c05785ce319a45b91a632
Chris Bernt <christopher.bernt@duke.edu>
2014-11-16 05:33:55 -0500

    Changed input forms

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,4 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class RequestForm(Form):
-	street = StringField('street', description = "Street", validators=[DataRequired()])
-	state = StringField('state', description = "State", validators=[DataRequired()])
-	zipCode = StringField('zipCode', description = "Zip Code", validators=[DataRequired()])
-	desc = StringField('desc', description = "Situacion", validators=[DataRequired()])
+	inputText = TextAreaField('inputText', validators=[DataRequired()])
